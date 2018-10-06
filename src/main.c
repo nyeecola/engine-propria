509ea4701a2b6f99d457db952c52697a75a43b32
lcagustini <torresmon235@gmail.com>
2018-10-06 15:40:36 -0300

    Nuklear

--- src/main.c
+++ src/main.c
@@ -9,6 +9,24 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#include "nuklear.h"
+#include "nuklear_glfw_gl3.h"
+
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
+
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
 static const struct {
     float x, y;
     float r, g, b;
@@ -20,8 +38,7 @@ static const struct {
 };
 
 // TODO: caller must free buffer
-char* load_file(char const* path)
-{
+char* load_file(char const* path) {
     char* buffer = 0;
     long length;
     FILE * f = fopen (path, "rb"); //was "rb"
@@ -50,20 +67,19 @@ char* load_file(char const* path)
     return buffer;
 }
 
-static void error_callback(int error, const char* description)
-{
+static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 int main(void) {
-
     GLFWwindow* window;
+    struct nk_context *ctx;
+    struct nk_colorf bg;
 
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
 
@@ -74,8 +90,12 @@ int main(void) {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Engine Propria", NULL, NULL);
 
     if (!window) {
         glfwTerminate();
@@ -85,7 +105,14 @@ int main(void) {
     glfwSetKeyCallback(window, key_callback);
     glfwMakeContextCurrent(window);
 
+    glewExperimental = 1;
     if (glewInit() != GLEW_OK) exit(EXIT_FAILURE);
+
+    ctx = nk_glfw3_init(window, NK_GLFW3_INSTALL_CALLBACKS);
+
+    struct nk_font_atlas *atlas;
+    nk_glfw3_font_stash_begin(&atlas);
+    nk_glfw3_font_stash_end();
 
     // display OpenGL context version
     {
@@ -163,9 +190,12 @@ int main(void) {
     }
 
     while (!glfwWindowShouldClose(window)) {
+        puts("sadasd");
         {
             int err = glGetError();
-            if (err) printf("%d\n", err);
+            if (err) {
+                printf("%d\n", err);
+            }
         }
 
         float ratio;
@@ -176,7 +206,43 @@ int main(void) {
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
+#if 1
+        nk_glfw3_new_frame();
+        if (nk_begin(ctx, "Engine Propria", nk_rect(50, 50, 230, 250),
+                    NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+                    NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        {
+            enum {EASY, HARD};
+            static int op = EASY;
+            static int property = 20;
+            nk_layout_row_static(ctx, 30, 80, 1);
+            if (nk_button_label(ctx, "button"))
+                fprintf(stdout, "button pressed\n");
 
+            nk_layout_row_dynamic(ctx, 30, 2);
+            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+
+            nk_layout_row_dynamic(ctx, 25, 1);
+            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+            nk_layout_row_dynamic(ctx, 20, 1);
+            nk_label(ctx, "background:", NK_TEXT_LEFT);
+            nk_layout_row_dynamic(ctx, 25, 1);
+            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
+                nk_layout_row_dynamic(ctx, 120, 1);
+                bg = nk_color_picker(ctx, bg, NK_RGBA);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
+                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
+                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
+                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
+                nk_combo_end(ctx);
+            }
+        }
+        nk_end(ctx);
+        nk_glfw3_render(NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+#endif
         glm_mat4_identity(m);
         vec3 axis = {0, 0, 1};
         glm_rotate(m, (float) glfwGetTime(), axis);
