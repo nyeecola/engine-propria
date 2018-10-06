5b06ccc8e0a4f038d70e3ae9b6a2a728d51b3ff2
lcagustini <torresmon235@gmail.com>
2018-10-06 16:11:51 -0300

    Fixed nuklear integration

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <cglm/cglm.h>
-
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 
 #define NK_INCLUDE_FIXED_TYPES
 #define NK_INCLUDE_STANDARD_IO
@@ -79,7 +85,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 int main(void) {
     GLFWwindow* window;
     struct nk_context *ctx;
-    struct nk_colorf bg;
+    struct nk_colorf bg = {0};
 
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
 
@@ -177,6 +183,7 @@ int main(void) {
     glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*) 0);
     glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*) (sizeof(float) * 2));
+    glBindVertexArray(0);
 
     // test code for opening editor on a specific file
     int a = fork();
@@ -190,7 +197,6 @@ int main(void) {
     }
 
     while (!glfwWindowShouldClose(window)) {
-        puts("sadasd");
         {
             int err = glGetError();
             if (err) {
@@ -206,57 +212,65 @@ int main(void) {
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
-#if 1
-        nk_glfw3_new_frame();
-        if (nk_begin(ctx, "Engine Propria", nk_rect(50, 50, 230, 250),
-                    NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-                    NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+
         {
-            enum {EASY, HARD};
-            static int op = EASY;
-            static int property = 20;
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                fprintf(stdout, "button pressed\n");
+            glBindVertexArray(vao);
 
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+            glm_mat4_identity(m);
+            vec3 axis = {0, 0, 1};
+            glm_rotate(m, (float) glfwGetTime(), axis);
+            //glm_ortho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f, p);
+            float left = -width / 2.0;
+            float right = width / 2.0;
+            float top = height / 2.0;
+            float bottom = -height / 2.0;
+            glm_ortho(left, right, top, bottom, 1.f, -1.f, p);
+            glm_mat4_mul(p, m, mvp);
 
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+            glUseProgram(program);
+            glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp[0]);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
 
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                bg = nk_color_picker(ctx, bg, NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
-                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
-                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
-                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
-                nk_combo_end(ctx);
-            }
+            glBindVertexArray(0);
         }
-        nk_end(ctx);
-        nk_glfw3_render(NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
-#endif
-        glm_mat4_identity(m);
-        vec3 axis = {0, 0, 1};
-        glm_rotate(m, (float) glfwGetTime(), axis);
-        //glm_ortho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f, p);
-        float left = -width / 2.0;
-        float right = width / 2.0;
-        float top = height / 2.0;
-        float bottom = -height / 2.0;
-        glm_ortho(left, right, top, bottom, 1.f, -1.f, p);
-        glm_mat4_mul(p, m, mvp);
+        {
+            nk_glfw3_new_frame();
+            if (nk_begin(ctx, "Engine Propria", nk_rect(50, 50, 230, 250),
+                        NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+                        NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE));
+            {
+                enum {EASY, HARD};
+                static int op = EASY;
+                static int property = 20;
+                nk_layout_row_static(ctx, 30, 80, 1);
+                if (nk_button_label(ctx, "button"))
+                    fprintf(stdout, "button pressed\n");
 
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+                nk_layout_row_dynamic(ctx, 30, 2);
+                if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+                if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+
+                nk_layout_row_dynamic(ctx, 25, 1);
+                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+                nk_layout_row_dynamic(ctx, 20, 1);
+                nk_label(ctx, "background:", NK_TEXT_LEFT);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
+                    nk_layout_row_dynamic(ctx, 120, 1);
+                    bg = nk_color_picker(ctx, bg, NK_RGBA);
+                    nk_layout_row_dynamic(ctx, 25, 1);
+                    bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
+                    bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
+                    bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
+                    bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
+                    nk_combo_end(ctx);
+                }
+            }
+
+            nk_end(ctx);
+            nk_glfw3_render(NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
